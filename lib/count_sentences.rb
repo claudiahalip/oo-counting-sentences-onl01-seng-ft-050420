348556f900d83e99b4384bec51faa91beb6a29b8
claudia v <claudia4ro@yahoo.com>
2020-05-10 17:00:38 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -33,7 +33,7 @@ class String
   end
 
   def count_sentences
-    array = self.split(/[?,!, .]/)
+    array = self.split(/[?,!,.]/)
     count = 0 
     array.each do |word|
       if word.length != 0 
